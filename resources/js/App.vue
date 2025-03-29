8ee5147419994602bb11389215f160993f29f5eb
LUCIANO131231 <2018110461@udh.edu.pe>
2025-03-29 13:07:29 -0500

    integrando vue al laravel

---
+++ resources/js/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="container mt-5">
+      <h1>Mi Aplicación Laravel + Vue</h1>
+      <p>La instalación de Vue.js se ha completado correctamente.</p>
+    </div>
+  </template>
+
+  <script setup>
+  export default {
+    name: 'App'
+  }
+  </script>
